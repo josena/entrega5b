98dc0e29f190679a42e5a930d4c262b9f7de08d7
José Nabor Ramírez <jmorfin@ucol.mx>
2020-11-06 08:22:39 -0600

    primer commit
    Archivos iniciales de la actividad 9, esta a un 30%

---
+++ scripts/act9.js
@@ -0,0 +1,21 @@
+function operaciones(opcion){
+	switch (opcion){
+	case 1:
+    	alert("Es una suma");
+    	/*instruciones para realizar la suma*/
+    	break;
+    case 2:
+    	alert("Es una resta");
+    	/*instruciones para realizar la resta*/
+    	break;
+    case 3:
+    	alert("Es una división");
+    	/*instruciones para realizar la resta*/
+    	break;
+    case 4:
+    	alert("Es una multiplicación");
+    	/*instruciones para realizar la resta*/
+    	break;
+    }
+
+}
